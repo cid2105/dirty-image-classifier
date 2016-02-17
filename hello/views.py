8b11bf898531db422e6dc664e741a0a0281690d6
cid2105 <cole.ian.diamond@gmail.com>
2016-02-17 01:53:55 -0500

    added URL download of image

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import filesizeformat
 from .models import CNN
 from django.views.decorators.csrf import csrf_exempt
 import json
+import urllib, cStringIO
 
 
 CONTENT_TYPES = ['image', 'video']
@@ -15,14 +16,22 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def process_image(im):
+	if im != None:
+		return json.dumps({'porn': cnn.predict(im) })
+	else:
+		return json.dumps({'error': "resize image failed"})
+
 @csrf_exempt
 def classify(request):
-	if request.method == 'POST' and 'image' in request.FILES:
-		im = cnn.resize(request.FILES['image'])
-		if im != None:
-			return HttpResponse(json.dumps({'porn': cnn.predict(im) }), content_type="application/json")
-		else:
-			return HttpResponse(json.dumps({'error': "resize image failed"}), content_type="application/json")
+	if request.method == 'POST':
+		if 'image' in request.FILES:
+			im = cnn.resize(request.FILES['image'])
+			return HttpResponse(process_image(im), content_type="application/json")
+		if 'url' in request.POST:
+			file = cStringIO.StringIO(urllib.urlopen(URL).read())
+			im = Image.open(file)
+			return HttpResponse(process_image(im), content_type="application/json")
 	else:
 		return HttpResponse(json.dumps({'error': "No image received"}), content_type="application/json")
 
